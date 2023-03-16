d151d01f85f443ee7a6e86f50559f094d716e5d9
可可同学 <AndroidLeo@outlook.com>
2023-03-16 22:58:28 +0800

    restful入门案例

--- reco-bean/src/main/java/cn/bipark/reco/controller/UserController.java
+++ reco-bean/src/main/java/cn/bipark/reco/controller/UserController.java
@@ -2,14 +2,63 @@ package cn.bipark.reco.controller;
 
 import cn.bipark.reco.domain.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * REST(Representational State Transfer),表现形式状态转换
+ * ■传统风格资源描述形式
+ * http://localhost/user/getById?id=1
+ * http://localhost/user/saveUser
+ * ■优点： 隐藏资源的访问行为，无法通过地址得知对资源是何种操作；书写简化。
+ * ■按照REST风格访问资源时使用行为动作区分对资源进行了何种操作
+ * http://localhost/users 查询全部用户信息GET(查询)
+ * http://localhost/users/1 查询指定用户信息GET(查询)
+ * http://localhost/users 添加用户信息 P0ST(新增/保存)
+ * http://localhost/users 修改用户信息 PUT(修改/更新)
+ * http://localhost/users/1 删除用户信息 DELETE(删除)
+ */
 @Controller
+//@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/users")
+    @ResponseBody
+    public String save() {
+        System.out.println("user save...");
+        return "{'module':'user save'}";
+    }
+
+    @DeleteMapping("/users/{id}")
+    @ResponseBody
+    public String delete(@PathVariable Integer id) {
+        System.out.println("user delete..." + id);
+        return "{'module':'user delete'}";
+    }
+
+    @PutMapping("/users")
+    @ResponseBody
+    public String update() {
+        System.out.println("user update...");
+        return "{'module':'user update'}";
+    }
+
+    @GetMapping("/users/{id}")
+    @ResponseBody
+    public String getById(@PathVariable Integer id) {
+        System.out.println("user getById..." + id);
+        return "{'module':'user getById'}";
+    }
+
+    @GetMapping("/users")
+    @ResponseBody
+    public String getAll() {
+        System.out.println("user getAll...");
+        return "{'module':'user getAll'}";
+    }
+
     //响应页面/跳转页面
     @RequestMapping("/toJumpPage")
     public String toJumpPage() {
@@ -40,6 +89,7 @@ public class UserController {
      * 位置：SpringMVC控制器方法定义上方
      * 作用：设置当前控制器返回值作为响应体
      * jackson-databind提供的转换json功能
+     *
      * @return
      */
     @RequestMapping("/toJsonList")
