585551b6b008e1d9b18ef8420d69e3edb4f89a11
可可同学 <AndroidLeo@outlook.com>
2023-03-16 22:36:57 +0800

    HttpMessageConverter类型转换器

--- reco-bean/src/main/java/cn/bipark/reco/controller/UserController.java
+++ reco-bean/src/main/java/cn/bipark/reco/controller/UserController.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
     //响应页面/跳转页面
     @RequestMapping("/toJumpPage")
-    public String toJumpPage(){
+    public String toJumpPage() {
         System.out.println("返回纯文本数据");
         return "page.jsp";
     }
@@ -17,18 +20,43 @@ public class UserController {
     //响应文本数据
     @RequestMapping("/toText")
     @ResponseBody
-    public String toText(){
+    public String toText() {
         System.out.println("返回纯文本数据");
         return "response text";
     }
 
     @RequestMapping("/toJsonPojo")
     @ResponseBody
-    public User toJsonPojo(){
+    public User toJsonPojo() {
         System.out.println("返回json对象数据");
         User user = new User();
         user.setUsername("可可");
         user.setPassword("0724");
         return user;
+    }
+
+    /**
+     * HttpMessageConverter类型转换器 数据转换
+     * 位置：SpringMVC控制器方法定义上方
+     * 作用：设置当前控制器返回值作为响应体
+     * jackson-databind提供的转换json功能
+     * @return
+     */
+    @RequestMapping("/toJsonList")
+    @ResponseBody
+    public List<User> toJsonList() {
+        System.out.println("返回json集合数据");
+        User user1 = new User();
+        user1.setUsername("可可");
+        user1.setPassword("0724");
+
+        User user2 = new User();
+        user2.setUsername("乐乐");
+        user2.setPassword("0520");
+
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        return userList;
     }
 }
