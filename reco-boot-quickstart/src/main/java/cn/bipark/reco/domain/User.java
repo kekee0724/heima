155fcf5eef83f293272e5c3a75b8134e900aeb2b
可可同学 <AndroidLeo@outlook.com>
2023-03-19 23:28:21 +0800

    id生成策略控制 全局设置

--- reco-boot-quickstart/src/main/java/cn/bipark/reco/domain/User.java
+++ reco-boot-quickstart/src/main/java/cn/bipark/reco/domain/User.java
@@ -1,6 +1,8 @@
 package cn.bipark.reco.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -11,8 +13,22 @@ import java.math.BigDecimal;
 public class User {
     //columns START
     /**
+     * 不同的表应用不同的id生成策略
+     * 日志：自增（1，2，3，4，…)
+     * 购物订单：特殊规则(FQ23948AK3843)
+     * 外卖单：关联地区日期等信息（1004202003143491)
+     * 关系表：可省略id
+     * ...
+     *
+     * AUTO(O)：使用数据库id自增策略控制id生成
+     * NONE(1)：不设置id生成策略
+     * INPUT(2)：用户手工输入id
+     * ASSIGN_ID(3)：雪花算法生成id（可兼容数值型与字符串型)
+     * ASSIGN_UUID(4)：以UUID生成算法作为id生成策略
+     *
      * id       db_column: id
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
     /**
      * 用户名       db_column: username
