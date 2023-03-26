fa99c6a782f7ad1213d4416363c2a087b3e7d3f8
可可同学 <AndroidLeo@outlook.com>
2023-03-26 11:12:26 +0800

    token刷新的拦截器

--- hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,7 +17,10 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate)).excludePathPatterns(
+        // token刷新的拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**");
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
                 "/shop/**",
                 "/voucher/**",
                 "/shop-type/**",
