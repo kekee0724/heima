1a97ad2b5ff35d5019eed6ecf302874986f345f7
可可同学 <AndroidLeo@outlook.com>
2023-04-01 14:13:58 +0800

    实现附近商户查询功能

--- hm-dianping/src/main/java/com/hmdp/service/IShopService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IShopService.java
@@ -18,4 +18,5 @@ public interface IShopService extends IService<Shop> {
 
     Result updateShop(Shop shop);
 
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
 }
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -7,15 +7,22 @@ import com.hmdp.utils.RedisIdWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 @Slf4j
 @SpringBootTest
@@ -28,6 +35,9 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
@@ -56,4 +66,31 @@ class HmDianPingApplicationTests {
         log.debug("time = {}", (end - begin));
     }
 
+    @Test
+    void loadShopData() {
+        // 1.查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2.把店铺分组，按照typeId分组，id一致的放到一个集含
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3，分批完成写入Redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1.获取类型id
+            Long typeId = entry.getKey();
+            String key = SHOP_GEO_KEY + typeId;
+            // 3.2.获取同类型的店铺的集合
+            List<Shop> shops = entry.getValue();
+            // 3.3.写入redis GEOADD key 经度 纬度 member
+            /*for (Shop shop : shops) {
+                stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+            }*/
+            List<RedisGeoCommands.GeoLocation<String>> locations = shops.stream()
+                    .map((shop ->
+                            new RedisGeoCommands.GeoLocation<>(
+                                    shop.getId().toString(),
+                                    new Point(shop.getX(), shop.getY()))
+                            )
+                    ).collect(Collectors.toList());
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
+    }
 }
