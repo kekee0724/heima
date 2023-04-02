e8d1052786c5c7e53f801c29951b1b4d3f52100d
可可同学 <AndroidLeo@outlook.com>
2023-04-02 22:35:06 +0800

    SpringCloud: 服务远程调用

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,8 +2,12 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
 public class OrderService {
@@ -11,9 +15,19 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Resource
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.利用RestTemplate发起http请求，查询用户
+        // 2.1.Url路径
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        // 2.2.发送郑ttp请求，实现远程调用
+        User user = restTemplate.getForObject(url, User.class);
+        // 3.封装User
+        order.setUser(user);
         // 4.返回
         return order;
     }
