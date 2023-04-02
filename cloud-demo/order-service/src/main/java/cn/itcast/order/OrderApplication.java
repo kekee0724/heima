705c5a5c1f31b0e63512c98800920bf08e6cd31f
可可同学 <AndroidLeo@outlook.com>
2023-04-03 00:12:35 +0800

    SpringCloud: 负载均衡策略

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package cn.itcast.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,11 +19,23 @@ public class OrderApplication {
 
     /**
      * 创建RestTemplate并注入Spring容器
+     *
      * @return
      */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 通过定义IRule实现可以修改负载均衡规则，有两种方式：
+     * 代码方式：在order-service中的OrderApplication类中，定义一个新的IRule
+     * 配置文件方式：在order-service的application.yml文件中，添加新的配置也可以修改规则
+     * @return
+     */
+    @Bean
+    public IRule randomRule() {
+        return new RandomRule();
     }
 }
