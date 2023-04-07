bab8699b8ca8c484c190c1a200320000fedbed7d
可可同学 <AndroidLeo@outlook.com>
2023-04-08 01:52:34 +0800

    SpringCloud: hotel-demo

--- hotel-demo/src/main/java/cn/itcast/hotel/web/HotelController.java
+++ hotel-demo/src/main/java/cn/itcast/hotel/web/HotelController.java
@@ -6,6 +6,9 @@ import cn.itcast.hotel.service.IHotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("hotel")
 public class HotelController {
@@ -16,5 +19,15 @@ public class HotelController {
     @PostMapping("list")
     public PageResult search(@RequestBody RequestParams params) {
         return hotelService.search(params);
+    }
+
+    @PostMapping("filters")
+    public Map<String, List<String>> getFilters(@RequestBody RequestParams params) {
+        return hotelService.getFilters(params);
+    }
+
+    @GetMapping("suggestion")
+    public List<String> getSuggestion(@RequestParam("key") String key) {
+        return hotelService.getSuggestion(key);
     }
 }
